05b795b90ff4d68b7a6d9c9721180c4be7fe7aa7
Ubuntu <ubuntu@ip-172-31-4-125.us-east-2.compute.internal>
2024-04-05 03:32:25 +0000

    Revert "Fix #6782. Ensure SyncThread is started"
    This reverts commit 0b1b22852415ff11c42262faa9ed1ce9d89f524c.

--- vector/src/main/java/im/vector/app/core/di/ActiveSessionHolder.kt
+++ vector/src/main/java/im/vector/app/core/di/ActiveSessionHolder.kt
@@ -20,7 +20,6 @@ import android.content.Context
 import arrow.core.Option
 import im.vector.app.ActiveSessionDataSource
 import im.vector.app.core.extensions.configureAndStart
-import im.vector.app.core.extensions.startSyncing
 import im.vector.app.core.pushers.UnifiedPushHelper
 import im.vector.app.core.services.GuardServiceStarter
 import im.vector.app.features.call.webrtc.WebRtcCallManager
@@ -101,16 +100,10 @@ class ActiveSessionHolder @Inject constructor(
     }
 
     suspend fun getOrInitializeSession(startSync: Boolean): Session? {
-        return activeSessionReference.get()
-                ?.also {
-                    if (startSync && !it.syncService().isSyncThreadAlive()) {
-                        it.startSyncing(applicationContext)
-                    }
-                }
-                ?: sessionInitializer.tryInitialize(readCurrentSession = { activeSessionReference.get() }) { session ->
-                    setActiveSession(session)
-                    session.configureAndStart(applicationContext, startSyncing = startSync)
-                }
+        return activeSessionReference.get() ?: sessionInitializer.tryInitialize(readCurrentSession = { activeSessionReference.get() }) { session ->
+            setActiveSession(session)
+            session.configureAndStart(applicationContext, startSyncing = startSync)
+        }
     }
 
     fun isWaitingForSessionInitialization() = activeSessionReference.get() == null && authenticationService.hasAuthenticatedSessions()
